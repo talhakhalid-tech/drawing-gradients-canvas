018d0f12b67dbf37efc7d5b1762d68dee04ee37c
talhakhalid-tech <talhakhalid0323@gmail.com>
2020-08-30 18:48:10 +0500

    first commit

--- canvas-script.js
+++ canvas-script.js
@@ -2,39 +2,92 @@ window.onload = function () {
   //definitions
   const canvas = document.querySelector("canvas");
   canvas.width = 900;
-  canvas.height = 450;
+  canvas.height = 600;
   const context = canvas.getContext("2d");
 
-  //Linear Gradient
+  let d1x = 0.0;
+  let d2x = 0.1;
+  let d3x = 0.2;
+  let d4x = 0.3;
+  let d5x = 0.4;
+  let d6x = 0.5;
+  let d7x = 0.6;
+  let d8x = 0.7;
+  let d9x = 0.8;
+  let d10x = 0.9;
+  let d11x = 1.0;
 
-  // const linearGradient = context.createLinearGradient(100, 50, 800, 400);
-  // linearGradient.addColorStop(0, "blue");
-  // linearGradient.addColorStop(0.25, "orange");
-  // linearGradient.addColorStop(0.5, "white");
-  // linearGradient.addColorStop(0.75, "purple");
-  // linearGradient.addColorStop(1, "green");
+  animateFunction = () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-  //Radial Gradient
+    const radialGradient = context.createRadialGradient(
+      450,
+      300,
+      10,
+      450,
+      300,
+      350
+    );
 
-  const radialGradient = context.createRadialGradient(
-    450,
-    225,
-    100,
-    450,
-    225,
-    350
-  );
-  radialGradient.addColorStop(0, "blue");
-  radialGradient.addColorStop(0.25, "orange");
-  radialGradient.addColorStop(0.5, "white");
-  radialGradient.addColorStop(0.75, "purple");
-  radialGradient.addColorStop(1, "green");
+    d1x += 0.01;
+    d2x += 0.01;
+    d3x += 0.01;
+    d4x += 0.01;
+    d5x += 0.01;
+    d6x += 0.01;
+    d7x += 0.01;
+    d8x += 0.01;
+    d9x += 0.01;
+    d10x += 0.01;
+    d11x += 0.01;
+    if (d1x >= 1.0) {
+      d1x = 0;
+    } else if (d2x >= 1.0) {
+      d2x = 0;
+    } else if (d3x >= 1.0) {
+      d3x = 0;
+    } else if (d4x >= 1.0) {
+      d4x = 0;
+    } else if (d5x >= 1.0) {
+      d5x = 0;
+    } else if (d6x >= 1.0) {
+      d6x = 0;
+    } else if (d7x >= 1.0) {
+      d7x = 0;
+    } else if (d8x >= 1.0) {
+      d8x = 0;
+    } else if (d9x >= 1.0) {
+      d9x = 0;
+    } else if (d10x >= 1.0) {
+      d10x = 0;
+    } else if (d11x >= 1.0) {
+      d11x = 0;
+    }
 
-  // context.fillStyle = linearGradient;
-  context.fillStyle = radialGradient;
-  context.strokeStyle = "red";
-  context.lineWidth = 5;
-  context.rect(100, 50, 700, 350);
-  context.fill();
-  context.stroke();
+    radialGradient.addColorStop(d1x, "black");
+    radialGradient.addColorStop(d2x, "white");
+    radialGradient.addColorStop(d3x, "black");
+    radialGradient.addColorStop(d4x, "white");
+    radialGradient.addColorStop(d5x, "black");
+    radialGradient.addColorStop(d6x, "white");
+    radialGradient.addColorStop(d7x, "black");
+    radialGradient.addColorStop(d8x, "white");
+    radialGradient.addColorStop(d9x, "black");
+    radialGradient.addColorStop(d10x, "white");
+    radialGradient.addColorStop(d11x, "black");
+
+    const radian = Math.PI / 180;
+
+    context.beginPath();
+    context.fillStyle = radialGradient;
+    context.strokeStyle = radialGradient;
+    context.lineWidth = 5;
+    context.arc(450, 300, 300, 0, radian * 360);
+    context.fill();
+    context.stroke();
+
+    window.requestAnimationFrame(animateFunction);
+  };
+
+  window.requestAnimationFrame(animateFunction);
 };
